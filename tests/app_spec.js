1d459a854984f411d2be768b2248dc79fc9559e5
vitogit <vitogit@users.noreply.github.com>
2016-11-03 16:37:12 -0300

    Setup basic app mocha testing

---
+++ tests/app_spec.js
@@ -0,0 +1,24 @@
+describe('App', function() {
+  
+  beforeEach(function() {
+    app = new App(); 
+  });
+    
+  it('exists', function() {
+    expect(app).to.exist;  
+  }); 
+  
+  it('filters the text', function() {
+
+  }); 
+    
+  it('parse the hashtags', function() {
+    var originalText = "Hello #hashtag. Bye #hashtag2"
+    
+  }); 
+      
+  it('extract the hashtags', function() {
+
+  });  
+
+});
