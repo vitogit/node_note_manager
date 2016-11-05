1e5d98cf4e825ca5356e27a80ac62d0a6a833027
vito <vitogit@users.noreply.github.com>
2016-11-05 00:37:04 -0300

    Add saveBackup and cleanNoteFiles methods

--- server.js
+++ server.js
@@ -7,32 +7,71 @@ app.use(express.static(__dirname + '/public'));
 //todo probably this needs to be a post
 app.get('/saveNotes',function(req,res){
   var notes = req.query.notes
-  saveToFile('savedNotes/notes.html',notes)
+  saveToFile(notesFolder+'notes.html',notes, function(){})
   //todo send error
   res.end("ok");
 });
 
 app.get('/loadNotes',function(req,res){
-  var data = loadFromFile('savedNotes/notes.html')
+  var data = loadFromFile(notesFolder+'notes.html')
   //todo send error
   res.end(data);  
 });
 
+app.get('/getNotesFiles',function(req,res){
+  var data = getNotesFiles();
+  //todo send error
+  res.end(JSON.stringify(data));  
+});
+
 app.listen(process.env.PORT || 5000);
 
+var notesFolder = 'savedNotes/';
+
 //PRIVATE 
-function saveToFile(filename, content) {
+function saveToFile(filename, content, done) {
   fs.writeFile(filename, content, function(err) {
     if(err) {
       return console.log(err);
       throw err;
     }
+    done();
     console.log("The file was saved!");
   });   
+}
+
+function saveBackup(filename, content, done) {
+  console.log(done)
+  var files = getNotesFiles()
+  var backupCount = files.length -1;
+  var number = backupCount+1;
+  var newFilename = notesFolder+filename+'.backup.'+number
+  saveToFile(newFilename, content, done)
 }
 
 function loadFromFile(filename) {
   return fs.readFileSync(filename, 'utf8');
 }
 
-module.exports = app
+function getNotesFiles() {
+  var notesFiles = [];
+  var files = fs.readdirSync(notesFolder);  
+  files.forEach(file => {
+    var stats = fs.statSync(notesFolder+file);
+    var size = stats["size"];
+    var modifiedDate = stats["mtime"];
+    notesFiles.push({name:file, size:size, modifiedDate:modifiedDate});
+  });
+  return notesFiles;
+}
+
+function cleanNoteFiles() {
+  var notesFiles = [];
+  var files = fs.readdirSync(notesFolder);  
+  files.forEach(file => {
+    fs.unlinkSync(notesFolder+file);
+  });
+}
+
+
+module.exports = {app, getNotesFiles, saveToFile, loadFromFile,saveBackup,cleanNoteFiles}
--- tests/server_spec.js
+++ tests/server_spec.js
@@ -3,11 +3,16 @@ var expect = chai.expect;
 var chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 var server = require('../server');
+var app = server.app
 
+before(function() {
+  server.cleanNoteFiles();
+});
+    
 //todo: this overwrite the current notes. create different envs for test?
 describe('Server', function() {
   it('save the notes GET', function(done) {
-    chai.request(server)
+    chai.request(app)
       .get('/saveNotes')
       .query({notes: 'hello'}) 
       .end(function(err, res){
@@ -19,7 +24,7 @@ describe('Server', function() {
   
   //todo: this uses the results from the previous tests. better make them independent
   it('load the notes GET', function(done) {
-    chai.request(server)
+    chai.request(app)
       .get('/loadNotes')
       .end(function(err, res){
         expect(res).to.have.status(200);
@@ -27,4 +32,46 @@ describe('Server', function() {
         done();
       });
   });
+  
+  it('get the notes files array', function() {
+    var files = server.getNotesFiles()
+    expect(files[0].name).to.eq('notes.html');    
+  }); 
+  
+  
+  it('manage backups: create a new backup file', function(done) {
+    server.saveBackup('notes.html', 'hello', function(){
+      var files = server.getNotesFiles()
+      expect(files.length).to.eq(2);   
+      expect(files[0].name).to.eq('notes.html');    
+      expect(files[1].name).to.eq('notes.html.backup.1');      
+      done();
+    })
+  });   
+
+  it('clean all notes files', function() {
+    server.cleanNoteFiles()
+    var files = server.getNotesFiles()
+    
+    expect(files.length).to.eq(0);
+  });     
+  // it('save until 10 backups', function(done) {
+  //   for (var i=0 ; i< 12 ; i++) {
+  //     chai.request(server)
+  //       .get('/saveNotes')
+  //       .query({notes: 'hello'}) 
+  //       .end(function(err, res){
+  //       });
+  //   }
+  //   chai.request(server)
+  //     .get('/getNotesFiles')
+  //     .end(function(err, res){
+  //       var files = JSON.parse(res.text)
+  //       expect(files[0].name).to.eq('notes.html');
+  //       expect(files[9].name).to.eq('notes.backup.9.html');
+  //       expect(files.size).to.eq(10);
+  //       done();
+  // 
+  //     });
+  // });    
 });
